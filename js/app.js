c5c8ea7a62adef4bb81fe594e7aa99c6452ef94b
interika <interika11@gmail.com>
2019-11-04 01:18:59 +0200

    full site layout

---
+++ js/app.js
@@ -0,0 +1,59 @@
+$(function(){
+
+    
+    $(".works-carousel").owlCarousel({
+        nav: true,
+        loop: true,
+        startPosition: 1,
+        dots: false,
+        margin: 30,
+        navText: ['<img src="i/left.svg">','<img src="i/right.svg">'],
+       responsive:{
+            0:{
+                items:1
+            },
+            1000:{
+                items:1.3
+            }
+       }
+    });
+
+
+
+    $('ul.work-tabs').on('click', 'li:not(.active)', function(e){
+        e.preventDefault();
+    $(this).addClass('active').siblings().removeClass('active')
+    .closest('section.tabs').find('div.tabs__content').removeClass('active')
+    });
+
+    $('.works-wrap').isotope({
+        itemSelector: '.works-wrap__item',
+      });
+
+      $(document).on("click", ".work-tabs__item", e => {
+        e.preventDefault();
+        e.stopPropagation();
+        const filter = "." + $(e.target).data("filter");
+        $('.works-wrap').isotope({ filter });
+    }); 
+
+
+
+    $(".contributor-carousel").owlCarousel({
+        nav: true,
+        loop: true,
+        dots: false,
+        margin: 0,
+        navText: ['<img src="i/left.svg">','<img src="i/right.svg">'],
+        responsive:{
+            0:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+       }      
+    });
+
+
+});
